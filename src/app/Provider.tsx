f842d9034bac6a17494acf4bceb98aa6c4f2b4e2
Nathaniel Tucker <me@ntucker.me>
2024-07-24 11:37:05 +0200

    Use latest getDefaultManagers config

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -1,30 +1,8 @@
 'use client';
-import StreamManager from '@/resources/StreamManager';
-import { Ticker } from '@/resources/Ticker';
-import { DevToolsManager, getDefaultManagers } from '@data-client/react';
 import { DataProvider } from '@data-client/react/nextjs';
+import getManagers from './getManagers';
 
-const getManagers =
-  process.env.NODE_ENV === 'development' ?
-    () => [
-      ...getDefaultManagers().filter(
-        mgr => mgr.constructor.name !== 'DevToolsManager',
-      ),
-    ]
-  : getDefaultManagers;
-
-const managers =
-  typeof window === 'undefined' ? getManagers() : (
-    [
-      new StreamManager(
-        () => new WebSocket('wss://ws-feed.exchange.coinbase.com'),
-        {
-          ticker: Ticker,
-        },
-      ),
-      ...getManagers(),
-    ]
-  );
+const managers = getManagers();
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   return <DataProvider managers={managers}>{children}</DataProvider>;
---
+++ src/app/getManagers.ts
@@ -0,0 +1,26 @@
+'use client';
+import { getDefaultManagers, actionTypes } from '@data-client/react';
+import StreamManager from '@/resources/StreamManager';
+import { Ticker } from '@/resources/Ticker';
+
+export default function getManagers() {
+  const mgrs = getDefaultManagers({
+    devToolsManager: {
+      // double latency to help with high frequency updates
+      latency: 1000,
+      // skip websocket updates as these are too spammy
+      predicate: (state, action) =>
+        action.type !== actionTypes.SET_TYPE || action.schema !== Ticker,
+    },
+  });
+  // only open stream client-side
+  if (typeof window !== 'undefined') {
+    mgrs.unshift(
+      new StreamManager(
+        () => new WebSocket('wss://ws-feed.exchange.coinbase.com'),
+        { ticker: Ticker },
+      ),
+    );
+  }
+  return mgrs;
+}
